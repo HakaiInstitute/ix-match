654810c6ec3bbdc74e15b421704b71de4006e01f
Taylor Denouden <taylor.denouden@hakai.org>
2024-09-06 12:18:50 -0700

    Update CLI to use new lib functions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use anyhow::Result;
 use clap::Parser;
-use polars::prelude::*;
 
 use std::path::PathBuf;
 use std::time::Duration;
 
-use ix_match::{find_dir_by_pattern, find_files, make_iiq_df, move_files};
+use ix_match::{find_dir_by_pattern, find_files, make_iiq_df, move_files, process_images};
 
 /// Match RGB and NIR IIQ files and move unmatched images to a new subdirectory.
 /// Helps to sort images from an aerial survey using PhaseOne cameras as a preprocessing step for
@@ -48,82 +47,16 @@ fn main() -> Result<()> {
     let nir_dir = find_dir_by_pattern(&iiq_dir, &args.nir_pattern)
         .ok_or_else(|| anyhow::anyhow!("NIR directory not found"))?;
 
-    // Find IIQ files
-    let rgb_iiq_files = find_files(&rgb_dir, ".iiq")?;
-    let nir_iiq_files = find_files(&nir_dir, ".iiq")?;
-
-    // Create dataframes
-    let rgb_df = make_iiq_df(&rgb_iiq_files)?;
-    let nir_df = make_iiq_df(&nir_iiq_files)?;
-
-    // Do the join
-    let joint_df = ix_match::join_dataframes(&rgb_df, &nir_df)?;
-
-    // Split df into matched and unmatched based on threshold
-    let thresh = Duration::from_millis(args.thresh).as_nanos() as i64;
-    let thresh_exp = lit(thresh).cast(DataType::Duration(TimeUnit::Nanoseconds));
-
-    let matched_df = joint_df
-        .clone()
-        .lazy()
-        .filter(col("dt").lt_eq(thresh_exp.clone()))
-        .collect()?;
-
-    let unmatched_rgb_df = joint_df
-        .clone()
-        .lazy()
-        .filter(col("dt").gt(thresh_exp.clone()))
-        .select(&[col("Path_rgb")])
-        .drop_nulls(None)
-        .collect()?;
-
-    let unmatched_nir_df = joint_df
-        .clone()
-        .lazy()
-        .filter(col("dt").gt(thresh_exp.clone()))
-        .select(&[col("Path_nir")])
-        .drop_nulls(None)
-        .collect()?;
-
-    if !args.dry_run {
-        // Create the unmatched directories
-        let unmatched_rgb_dir = rgb_dir.join("unmatched");
-        std::fs::create_dir_all(&unmatched_rgb_dir)?;
-
-        let unmatched_nir_dir = nir_dir.join("unmatched");
-        std::fs::create_dir_all(&unmatched_nir_dir)?;
-
-        // Move all matched iiq files to camera dirs root
-        move_files(&matched_df, &rgb_dir, "Path_rgb", args.verbose)?;
-        move_files(&matched_df, &nir_dir, "Path_nir", args.verbose)?;
-
-        // Move unmatched files
-        if unmatched_rgb_df.height() > 0 {
-            std::fs::create_dir_all(&unmatched_rgb_dir)?;
-            move_files(
-                &unmatched_rgb_df,
-                &unmatched_rgb_dir,
-                "Path_rgb",
-                args.verbose,
-            )?;
+    let thresh = Duration::from_millis(args.thresh);
+    match process_images(&rgb_dir, &nir_dir, thresh, args.dry_run, args.verbose) {
+        Ok((rgb_count, nir_count, matched_count)) => {
+            println!(
+                "RGB: {}, NIR: {} ({} match)",
+                rgb_count, nir_count, matched_count
+            );
         }
-        if unmatched_nir_df.height() > 0 {
-            std::fs::create_dir_all(&unmatched_nir_dir)?;
-            move_files(
-                &unmatched_nir_df,
-                &unmatched_nir_dir,
-                "Path_nir",
-                args.verbose,
-            )?;
-        }
+        Err(e) => eprintln!("Error: {}", e),
     }
-
-    println!(
-        "RGB: {}, NIR: {} ({} match)",
-        rgb_df.height(),
-        nir_df.height(),
-        matched_df.height()
-    );
 
     Ok(())
 }
